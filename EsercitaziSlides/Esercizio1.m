f310680742dbfe95bb806835896106fc7b4fcc8d
iskorini <iskorini@gmail.com>
2018-06-30 16:26:40 +0200

    es 2 e 3, modifica esercizio 1

--- EsercitaziSlides/Esercizio1.m
+++ EsercitaziSlides/Esercizio1.m
@@ -1,25 +1,30 @@
-a = 0; b = 1; n = 4;
+clear;
+a = 0; b = 2; n = 20;
 precision = 0.0001;
 u = a:precision:b;
-x = zeros(1, length(a:precision:b));
-y = zeros(1, length(a:precision:b));
+r_u = (u-a)./(b-a);
 [x_p, y_p] = ginput(n+1);
+p_x = zeros(1, length(u));
+p_y = zeros(1, length(u));
 for i = 1:length(u)
-    [x(i), y(i)] = de_casteljau(n, x_p, y_p, u(i));
+    [t_x, t_y] = de_casteljau(n, x_p, y_p, r_u(i));
+    p_x(i) = t_x(n+1, n+1); 
+    p_y(i) = t_y(n+1, n+1);
 end
-plot(x_p, y_p, '-O');
-hold on;
-plot(x, y, 'linewidth',3);
-hold on;
-function [x,y] = de_casteljau(n, x_p, y_p, u)
-    Q_x = x_p;
-    Q_y = y_p;
-    for k = 0:n
-        for i = 1:n-k
-            Q_x(i) = (1-u)*Q_x(i)+u*Q_x(i+1);
-            Q_y(i) = (1-u)*Q_y(i)+u*Q_y(i+1);
+% 
+% 
+plot(x_p, y_p, '-O'); hold on;
+plot(p_x, p_y, 'linewidth',3); hold on;
+
+function [Q_x,Q_y] = de_casteljau(n, x_p, y_p, u)
+    Q_x = zeros(n+1, n+1);
+    Q_y = zeros(n+1, n+1);
+    Q_x(:,1) = x_p;
+    Q_y(:,1) = y_p;
+    for i = 2:n+1
+        for j = i:n+1
+            Q_x(j,i) = (1-u)*Q_x(j-1, i-1)+u*Q_x(j, i-1);
+            Q_y(j,i) = (1-u)*Q_y(j-1, i-1)+u*Q_y(j, i-1);
         end
     end
-    x = Q_x(1);
-    y = Q_y(1);
 end
