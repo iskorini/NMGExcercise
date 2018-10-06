fa5cfda937ae1b2ccd979040f279d9d965559578
iskorini <iskorini@gmail.com>
2018-10-06 15:49:46 +0200

    Aggiunte figure, parte iniziale relazione

--- Giannelli/Relazione/code/bspline_property.m
+++ Giannelli/Relazione/code/bspline_property.m
@@ -1,7 +1,7 @@
 clear;
 k = 4;
 %knots = augknt([0 1 2 3 4 5], k); 
-knots = [0 0 0 1 1 2 3 3 3];
+knots = [0 1 2 3 4 4 5 6 7 8 8];
 tau = knots(1):0.001:knots(end);
 c = spcol(knots, k, tau);
 for i = 1:length(knots)-k
@@ -12,8 +12,8 @@ end
 %Supporto locale: N_{i, r}(t) = 0 se t \notin [t_i, t_{i+r}]
 %considero la spline con i = 1, t_1 = 0, t_5 = 1, al di fuori di questo
 %intervallo vale 0:
-plot(tau, c(:, 1), 'linew', 3); hold on;
-c1 = spcol(knots, k, 0.95:0.01:1.05);
+c1 = spcol(knots, k, 0.85:0.0001:1.15);
+%plot(0.85:0.0001:1.15, c1(:, 1), 'linew', 3); hold on;
 disp(c1);
 
 %Non negatività: N_{i, r}(t) \geq 0 \forall t \in \mathbb{R}
@@ -30,4 +30,5 @@ end
 %\sum_{i = 0}^{n+k-r} = 1 \forall t \in [t_{r-1}, t_{n+1+k-r}] r = 1 \dots k
 %in questo caso abbiamo k = r, quindi 
 %\sum_{i = 0}^{n} = 1 \forall t \in [t_{r-1}, t_{n+1+k-r}] r = 1 \dots k
-plot(tau, sum(c,2), '--', 'linewidth', 4);
+plot(tau, sum(c,2), '-', 'linewidth', 4);
+
